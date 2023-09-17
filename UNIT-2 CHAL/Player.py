f5c1cfd3f3ff7cc2451588c29af024debe0bd534
treradsfdgefg <145260598+treradsfdgefg@users.noreply.github.com>
2023-09-17 16:03:12 +0530

    Add files via upload

---
+++ UNIT-2 CHAL/Player.py
@@ -0,0 +1,20 @@
+class player():
+  def play(self):
+    print("the player is playing cricker")
+   
+class Batsman(player):
+   def play(self):
+       print("the batsman is batting")
+     
+     
+class Bowler(player):
+  def play(self):
+      print("the bowler is bowling")
+batsman=Batsman()
+bowler=Bowler()
+batsman.play()
+bowler.play()
+
+
+      
+    
